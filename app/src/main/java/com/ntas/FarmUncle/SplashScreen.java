dc5d6c14b45e23add94f95e91632998fad3c9e2f
Paras Jain <parasj221@gmail.com>
2020-02-05 22:01:39 +0530

    Commit

---
+++ app/src/main/java/com/ntas/FarmUncle/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.ntas.FarmUncle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =new Intent(SplashScreen.this, Welcome.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, 500);
+
+
+    }
+}
